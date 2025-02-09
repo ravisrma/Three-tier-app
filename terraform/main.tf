597239f323f5f20f1b0dfbe5263a8499203dfbc6
Ravi sharma <67187238+ravisrma@users.noreply.github.com>
2025-02-09 17:02:19 +0530

    add elk

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ provider "kubernetes" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.aws_eks_cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
