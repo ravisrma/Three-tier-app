b7285cd4a8f9ce7b33cc3a19b4ed0bc4c92f7731
Ravi sharma <67187238+ravisrma@users.noreply.github.com>
2025-02-09 16:47:51 +0530

    add elk

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,25 @@ module "iam" {
   source = "./modules/iam"
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.aws_eks_cluster.cluster.name
+}
+
+resource "kubernetes_namespace" "elk" {
+  metadata {
+    name = "elk"
+  }
+}
 
 # Creating EKS Cluster
 module "eks" {
