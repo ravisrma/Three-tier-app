536ede83e7b9ad642b6756e90b82ebefe2375ad8
Ravi sharma <67187238+ravisrma@users.noreply.github.com>
2025-02-11 15:45:47 +0530

    remove namespace

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,10 +1,10 @@
-output "cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
-}
+# output "cluster_endpoint" {
+#   value = aws_eks_cluster.eks.endpoint
+# }
 
-output "cluster_certificate_authority_data" {
-  value = aws_eks_cluster.eks.certificate_authority[0].data
-}
+# output "cluster_certificate_authority_data" {
+#   value = aws_eks_cluster.eks.certificate_authority[0].data
+# }
 
 output "aws_eks_cluster_name" {
   value = aws_eks_cluster.eks.id
